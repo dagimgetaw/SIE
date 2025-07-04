1a76fec637addc50743a0ce175d84d1f83a7b757
Dagim <dagimgetaW27@gmail.com>
2025-07-04 10:08:03 +0300

    feat: build homepage with home, about, service and contact components

---
+++ client/src/Pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Home from "../Components/Home/Home";
+import About from "../Components/Home/About";
+import Service from "../Components/Home/Service";
+import Contact from "../Components/Home/Contact";
+
+export default function HomePage() {
+  return (
+    <>
+      <Home />
+      <About />
+      <Service />
+      <Contact />
+    </>
+  );
+}
